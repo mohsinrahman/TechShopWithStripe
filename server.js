b4d55d4b6bd38efd1bee267609c35fd273b82f15
juliamattson <julia.mattson@medieinstitutet.se>
2020-09-09 10:30:08 +0200

    added stripe base

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+require('dotenv').config('.env')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const app = express();
+
+app.use('/api', express.json())
+
+app.use(express.static('public'))
+
+app.get('/v1/products', async (req, res) => {
+
+    const products = await stripe.products.list({
+    limit: 3,
+    });
+    console.log(products)
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
